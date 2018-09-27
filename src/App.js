5d3376f091592389f2feb249ff06040a3e1ffc7d
hamadou diallo <hmaadou_diallo@yahoo.com>
2018-09-27 12:18:19 -0400

    navbar

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Switch, Route, Router } from 'react-router-dom'
+import Home from './Home'
+import Navigation from './stateless/Navigation'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Navigation />
+        <div className="App">
+          <header className="Hamadou Tidiane Diallo Portfolio">
+            <h1 className="App-title">Welcome To Hamadou Tidiane Diallo Homepage</h1>
+          </header>
+        </div>
       </div>
     );
   }
